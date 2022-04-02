fbba735d6a08151adcf8eb9702ee36b16cca41e5
akhileshvj <akhilesh75vijay@gmail.com>
2022-04-02 19:21:27 +0530

    first commit

--- firstinstance.tf
+++ firstinstance.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "Myfirstinstance" {
   ami = "ami-04505e74c0741db8d"
   instance_type = "t2.micro"
   tags = {
-    Name = "threedemo"
+    Name = "demoinstance-${count.index}"
   }
 }
