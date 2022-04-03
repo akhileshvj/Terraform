62cfbd3e0d2348838d3cb8eca1595314a7ab193b
akhileshvj <akhilesh75vijay@gmail.com>
2022-04-03 20:19:26 +0530

    first commit

--- TerraformConceptBuild/variables.tf
+++ TerraformConceptBuild/variables.tf
@@ -14,7 +14,7 @@ variable "Security_Group"{
 variable "AMIS" {
   type = map(any)
   default = {
-    us_east_1 = "ami-04505e74c0741db8d"
+    us-east-1 = "ami-04505e74c0741db8d"
     us-west-2 = "ami-0352d5a37fb4f603f"
     us-west-1 = "ami-0f40c8f97004632f9"
   }
