833a15f069290509567b79e72032b9afac3c137f
akhileshvj <akhilesh75vijay@gmail.com>
2022-04-04 15:51:28 +0530

    first commit

--- UsingDatasource/instancedeploy.tf
+++ UsingDatasource/instancedeploy.tf
@@ -1,7 +1,16 @@
 data "aws_availability_zones" "available" {}
 
+data "aws_ami" "latest-ubuntu" {
+  most_recent = true
+  owners = ["063491364108"]
+  filter {
+    name   = "name"
+    values = ["ubuntu*"]
+  }
+}
+
 resource "aws_instance" "myInstance" {
-  ami = lookup(var.AMIS, var.AWS_REGION )
+  ami = data.aws_ami.latest-ubuntu.id
   instance_type = "t2.micro"
   availability_zone = data.aws_availability_zones.available.names[1]
   tags = {
