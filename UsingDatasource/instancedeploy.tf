dd096f201dc95b14e45b63f590657806ee0c4f4d
akhileshvj <akhilesh75vijay@gmail.com>
2022-04-04 16:26:30 +0530

    first commit

--- UsingDatasource/instancedeploy.tf
+++ UsingDatasource/instancedeploy.tf
@@ -1,6 +1,6 @@
 data "aws_availability_zones" "available" {}
 
-data "aws_ami" "latest-ubuntu" {
+data "aws_ami" "cheers"{
   most_recent = true
   owners = ["063491364108"]
   filter {
@@ -10,12 +10,11 @@ data "aws_ami" "latest-ubuntu" {
 }
 
 resource "aws_instance" "myInstance" {
-  ami = data.aws_ami.latest-ubuntu.id
+  ami = data.aws_ami.cheers.id
   instance_type = "t2.micro"
   availability_zone = data.aws_availability_zones.available.names[1]
   tags = {
     Name = "CustomImage"
   }
-
 
 }
